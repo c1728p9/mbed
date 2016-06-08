eb0e74761db80619992fe4f6d65fccbb5965a977
Russ Butler <russ.butler@arm.com>
2016-06-08 17:17:14 +0100

    Make FAT filesystem thread safe
    Add lock and unlock calls to the FAT filesystem so it is thread safe.

--- libraries/fs/fat/FATDirHandle.h
+++ libraries/fs/fat/FATDirHandle.h
@@ -23,18 +23,26 @@
 #define MBED_FATDIRHANDLE_H
 
 #include "DirHandle.h"
+#include "platform.h"
 
 using namespace mbed;
 
 class FATDirHandle : public DirHandle {
 
  public:
-    FATDirHandle(const FATFS_DIR &the_dir);
+    FATDirHandle(const FATFS_DIR &the_dir, rtos::Mutex * mutex);
     virtual int closedir();
     virtual struct dirent *readdir();
     virtual void rewinddir();
     virtual off_t telldir();
     virtual void seekdir(off_t location);
+
+ protected:
+
+    virtual void lock();
+    virtual void unlock();
+
+    rtos::Mutex * _mutex;
 
  private:
     FATFS_DIR dir;
