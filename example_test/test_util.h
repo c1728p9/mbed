4db214b4c1ac96fae95f4987188f12794096fbe6
Russ Butler <russ.butler@arm.com>
2018-05-01 19:46:30 -0500

    Add functional test for USB classes

---
+++ example_test/test_util.h
@@ -0,0 +1,25 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2018-2018 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TEST_UTIL_H
+#define TEST_UTIL_H
+
+void test_util_countdown(int count);
+void test_util_wait_line();
+void test_util_read_line(char *buf, uint32_t size);
+bool test_util_get_response();
+
+#endif
