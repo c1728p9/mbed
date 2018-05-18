488ecfb7125ca09ec42b6e118a7ec3484d1ad2b0
Russ Butler <russ.butler@arm.com>
2018-05-18 12:00:34 -0500

    Add test main for task queue

--- example_test/main.cpp
+++ example_test/main.cpp
@@ -1,18 +1,95 @@
 #include "mbed.h"
 
-int main_cdc();
-int main_serial();
-int main_keyboard();
-int main_mouse();
-int main_mouse_keyboard();
+//int main_cdc();
+//int main_serial();
+//int main_keyboard();
+//int main_mouse();
+//int main_mouse_keyboard();
+
+void cb1(void*)
+{
+
+    printf("It ran!\r\n");
+}
+
+void cb2(void*)
+{
+
+    printf("Test 2 failed!\r\n");
+}
+void cb3(void* q);
+
+uint32_t queue_buf[1];
+TaskBase event3((uint8_t*)queue_buf, sizeof(queue_buf), cb3);
+void cb3(void* q)
+{
+    EventQueue *queue = *(EventQueue **)q;
+    wait(1);
+    printf("SPAM!\r\n");
+    queue->post(&event3);
+}
+
+void print_number(void *num)
+{
+    printf("My number is %i\r\n", (int)num);
+}
 
 int main()
 {
-    main_cdc();
-    main_serial();
-    main_keyboard();
-    main_mouse();
-    main_mouse_keyboard();
+    printf("Test 1\r\n");
+    {
+    EventQueue queue;
+
+    TaskBase event1(NULL, 0, cb1);
+    TaskBase event2(NULL, 0, cb2);
+
+    EventQueue *ptr = &queue;
+    memcpy((void*)queue_buf, (void*)&ptr, sizeof(&queue));
+
+
+    queue.post(&event1);
+    queue.post(&event2);
+    event2.cancel();
+    queue.post(&event3);
+
+    Callback<void()> cb(print_number, (void*)7);
+    Task< Callback<void()> > task(cb);
+    queue.post(&task);
+
+    queue.dispatch();
+    }
+
+//    printf("Test 2\r\n");
+//    {
+//        EventQueue queue;
+//
+//        StaticEvent event(NULL, 0, cb);
+//
+//
+//        queue.post(&event);
+//        event.cancel();
+//
+//        queue.dispatch();
+//    }
+//
+//    printf("Test 3\r\n");
+//    {
+//        EventQueue queue;
+//
+//        StaticEvent event(NULL, 0, cb);
+//
+//
+//        queue.post(&event);
+//        queue.cancel(&event);
+//
+//        queue.dispatch();
+//    }
+
+//    main_cdc();
+//    main_serial();
+//    main_keyboard();
+//    main_mouse();
+//    main_mouse_keyboard();
 
     return 0;
 }
