90fc2a70f6af0579b699dd97d12ba8e2150f1980
Russ Butler <russ.butler@arm.com>
2018-05-23 19:36:56 -0500

    Add USBMSD manual test

--- example_test/main.cpp
+++ example_test/main.cpp
@@ -5,14 +5,17 @@ int main_serial();
 int main_keyboard();
 int main_mouse();
 int main_mouse_keyboard();
+int main_msd();
 
 int main()
 {
-    main_cdc();
-    main_serial();
-    main_keyboard();
-    main_mouse();
-    main_mouse_keyboard();
+    //main_cdc();
+    //main_serial();
+    //main_keyboard();
+    //main_mouse();
+    //main_mouse_keyboard();
+    //main_midi();
+    main_msd();
 
     return 0;
 }
